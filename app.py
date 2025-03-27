a00073fb0c535e9d80877bb38352db87786c84d4
mohdfarhan254 <mohdfarhan29102002@gmail.com>
2025-03-28 00:20:39 +0530

    Added predict_api endpoint & frontend

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 import pickle
 import numpy as np
 
 # Initialize Flask application
 app = Flask(__name__)
-
+CORS(app) 
 # Load the trained ML model from the .pkl file
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
